f66693dcdc2c896fdee8678220745ed30811f58f
andyxzdev <andrewsgamer123@outlook.com>
2025-11-12 20:38:58 -0300

    backend em java

---
+++ src/test/java/com/codeUp/demo/CodeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeUp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
